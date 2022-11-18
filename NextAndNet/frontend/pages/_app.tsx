5aa96668e5802bf9d935daf323ddc5c7c6f75136
JoshuaTheMiller <joshdan65@gmail.com>
2022-11-17 19:28:13 -0600

    Use NextAuth instead of custom nonsense
    * https://next-auth.js.org/
    
    * Potential TODO: make custom signin page as default is kind of ugly

--- NextAndNet/frontend/pages/_app.tsx
+++ NextAndNet/frontend/pages/_app.tsx
@@ -1,15 +1,17 @@
-import { MsalProvider } from '@azure/msal-react';
-import { msalInstance } from '../services/msal';
+import { SessionProvider } from "next-auth/react"
 import type { AppProps } from 'next/app'
 
 // https://nextjs.org/docs/advanced-features/custom-app
 // https://nextjs.org/docs/basic-features/typescript#custom-app
 function MyApp({ Component, pageProps }: AppProps) {
+  
+  const {session, pageProps:actualProps} = pageProps;
+
   return (
-    <MsalProvider instance={msalInstance}>
-      <Component {...pageProps} />
-    </MsalProvider>
-  );
+    <SessionProvider session={session}>
+      <Component {...actualProps} />
+    </SessionProvider>
+  )
 }
 
 export default MyApp;
--- NextAndNet/frontend/pages/index.tsx
+++ NextAndNet/frontend/pages/index.tsx
@@ -1,62 +1,14 @@
-import {
-  AuthenticatedTemplate,
-  UnauthenticatedTemplate,
-  useMsal,
-} from '@azure/msal-react';
 import { useEffect, useState } from 'react';
-
-function SignInButton() {  
-  const { instance } = useMsal();
-
-  const signInFunc = async () => {        
-    const accounts = instance.getAllAccounts();
-    if (accounts.length === 0) {
-        // No user signed in
-        instance.loginRedirect();
-    }
-  }
-
-  return (    
-    <button onClick={signInFunc}>Sign In</button>  
-  );
-}
-
-function WelcomeUser() {
-  const { accounts } = useMsal();
-  const username = accounts[0].username;
-
-  return <p>Welcome, {username}</p>;
-}
+import LoginButton from '../components/LoginButton';
 
 function IndexPage() {  
-  const { instance } = useMsal();
-
   const [isPrepped, setIsPrepped] = useState(false)  
-
-  useEffect(() => {   
-    console.log("Prepping") 
-    instance.handleRedirectPromise()
-    .then(() => {
-      setIsPrepped(true);
-      console.log("Prepped!");
-    })
-    .catch(() => console.log("sad"))   
-  }, [])
-
-  if (!isPrepped) return <p>Loading...</p>  
 
   return (  
     <>
       <h1>Hello!</h1>
 
-      <AuthenticatedTemplate>
-        <p>This will only render if a user is signed-in.</p>
-        <WelcomeUser />
-      </AuthenticatedTemplate>
-      <UnauthenticatedTemplate>
-        <p>This will only render if a user is not signed-in.</p>    
-        { isPrepped ? <SignInButton/> : <></>}               
-      </UnauthenticatedTemplate>
+      <LoginButton/>
     </>  
   );
 }
